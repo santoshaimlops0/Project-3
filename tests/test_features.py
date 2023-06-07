1e6f6020dcc1bb0ca433bfd68b0acd236347e905
santoshaimlops0 <132829094+santoshaimlops0@users.noreply.github.com>
2023-06-07 17:26:47 +0530

    Added one more test case to test_features to check WeekdayImputer

--- tests/test_features.py
+++ tests/test_features.py
@@ -11,13 +11,21 @@ import pdb
 
 import numpy as np
 from bike_share.config.core import config
-# from bike_share.processing.features import WeekdayImputer
+from bike_share.processing.features import WeekdayImputer
 # from tests.conftest import sample_input_data
 
 def test_Weekday_Is_Nan(sample_input_data):
     # Given
-    assert np.isnan(sample_input_data.loc[5,'weekday'])
+    assert np.isnan(sample_input_data[0].loc[5,'weekday'])
    
+    transformer = WeekdayImputer(
+        variables=config.model_config.weekday_var,  
+    )
+    # When
+    subject = transformer.fit(sample_input_data[0]).transform(sample_input_data[0])
+
+    # Then
+    assert subject.loc[3,'weekday'] == 'Thu'
 
 # if __name__ == "__main__":
 #     X_Test = sample_input_data()
